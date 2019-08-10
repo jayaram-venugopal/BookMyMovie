308937e13dd812c92848e1b5768bc2ad710cfade
Jayaram.V <jayaram.v@robosoftin.com>
2019-08-10 19:39:36 +0530

    Updated: UI Modifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def method_name
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root 'bookings#index', as: :authenticated_root
+  end
+
   root 'shows#index'
   # root 'bookings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :bookings, only: [:index]
+   
   resources :shows, only: [:index, :show] do
     collection do
-      get '/:id/seating', to: 'shows#seating'
+      get '/:id/seating', :to => 'shows#seating', :as => 'seating'
     end
   end
 end
